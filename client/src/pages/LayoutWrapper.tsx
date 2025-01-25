4a47b17aa9bdf12e8e5578eaa1058bc533d07716
rahmetasevic <ahmetasevicrasim1@gmail.com>
2025-01-25 09:36:10 +0100

    cleanup

--- client/src/pages/LayoutWrapper.tsx
+++ client/src/pages/LayoutWrapper.tsx
@@ -1,15 +1,14 @@
+import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 import { addTransition } from "@utils/index";
 
 import "./LayoutWrapper.scss";
-import { useEffect, useState } from "react";
 
 export const LayoutWrapper = () => {
 	const location = useLocation();
 
 	useEffect(() => {
-		console.log(location);
 		const outlet =
 			document.querySelector(".layout-wrapper")?.firstElementChild
 				?.classList[0];
